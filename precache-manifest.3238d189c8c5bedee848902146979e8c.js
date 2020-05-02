c003f012f22bf5c622269a4c3d3706d49b0b61fb
lavana555 <timofeevniki3@gmail.com>
2020-05-02 13:56:07 +0300

    Updates

---
+++ precache-manifest.3238d189c8c5bedee848902146979e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c4b4efb591a7f13355510fa5eca808",
+    "url": "/webcountries/index.html"
+  },
+  {
+    "revision": "d7972a6a06075527d8e9",
+    "url": "/webcountries/static/css/main.abc2416f.chunk.css"
+  },
+  {
+    "revision": "be0e6d4eb60798b24f30",
+    "url": "/webcountries/static/js/2.1a300693.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/webcountries/static/js/2.1a300693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7972a6a06075527d8e9",
+    "url": "/webcountries/static/js/main.13938ac0.chunk.js"
+  },
+  {
+    "revision": "f4136ceae9add3ccf28f",
+    "url": "/webcountries/static/js/runtime-main.58ab2016.js"
+  }
+]);
